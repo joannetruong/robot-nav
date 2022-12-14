01dad0af8905add6104c637a25acba9b6be624e6
Naoki Yokoyama <naoki.yokoyama7@gmail.com>
2022-12-15 07:01:17 +1300

    added setup.py for pip installing as a module

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name="kin_nav",
+    version="0.1",
+    author="Naoki Yokoyama",
+    author_email="naokiyokoyama@github",
+    description="Kinematic navigation training for habitat",
+    url="https://github.com/naokiyokoyama/kin_nav",
+    packages=setuptools.find_packages(),
+)
